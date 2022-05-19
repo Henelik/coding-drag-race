546d1511f8f48390ff72c61764164d5526a885dd
Henelik <kelyn@crandall.me>
2022-05-19 17:01:34 -0500

    add benchmark

---
+++ rule_110/go/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkCellularAutomata_CurrentRow(b *testing.B) {
+	b.StopTimer()
+
+	width = 1024000
+
+	ca := NewCellularAutomata(30, '0', ' ', width)
+	bytes := make([]byte, width)
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		bytes = ca.NextRow()
+	}
+
+	bytes = bytes[:]
+}
